34a942026773740fb44222d68a305393a1b3db08
matheus.barbosa <matheus.barbosa@valemobi.com.br>
2023-01-29 21:28:14 -0300

    Feat: Cria view da tela de settings em SwiftUI

--- RickAndMorty/Controllers/Core/RMSettingsViewController.swift
+++ RickAndMorty/Controllers/Core/RMSettingsViewController.swift
@@ -5,21 +5,48 @@
 //  Created by mathues barbosa on 02/01/23.
 //
 
+import SwiftUI
 import UIKit
 
 /// Controller to show various app options and settings
 final class RMSettingsViewController: UIViewController {
 
-    private let viewModel = RMSettingsViewViewModel(
-        cellViewModels: RMSettingsOption.allCases.compactMap({
-            return RMSettingsCellViewModel(type: $0)
-        })
+    private let settingsSwiftUIController = UIHostingController(
+        rootView: RMSettingsView(
+            viewModel: RMSettingsViewViewModel(
+                cellViewModels: RMSettingsOption.allCases.compactMap({
+                    return RMSettingsCellViewModel(type: $0)
+                })
+            )
+        )
     )
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .systemBackground
         self.title = "Settings"
+        addSwiftUIController()
+        setUpViews()
+        addConstraints()
+    }
+
+    private func addSwiftUIController() {
+        addChild(settingsSwiftUIController)
+        settingsSwiftUIController.didMove(toParent: self)
+        settingsSwiftUIController.view.translatesAutoresizingMaskIntoConstraints = false
+    }
+
+    private func setUpViews() {
+        view.addSubview(settingsSwiftUIController.view)
+    }
+
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            settingsSwiftUIController.view.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            settingsSwiftUIController.view.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            settingsSwiftUIController.view.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+            settingsSwiftUIController.view.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        ])
     }
 
 }
