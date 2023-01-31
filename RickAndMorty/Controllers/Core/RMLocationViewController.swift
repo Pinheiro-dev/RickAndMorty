a750fe119ba1541ea43a7c46062f92db53b97379
matheus.barbosa <matheus.barbosa@valemobi.com.br>
2023-01-31 15:46:34 -0300

    Feat: Cria RMLocationView e sua respectiva viewModel

--- RickAndMorty/Controllers/Core/RMLocationViewController.swift
+++ RickAndMorty/Controllers/Core/RMLocationViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 /// Controller to show and search for Locations
 final class RMLocationViewController: UIViewController {
 
+    private let primaryView = RMLocationView()
+
+    private let viewModel = RMLocationViewViewModel()
+
+    // MARK: - Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
@@ -21,9 +27,18 @@ final class RMLocationViewController: UIViewController {
                                                             action: #selector(didTapSearch))
     }
 
-    func setUpView() {}
+    func setUpView() {
+        view.addSubview(primaryView)
+    }
 
-    func addConstraints() {}
+    func addConstraints() {
+        NSLayoutConstraint.activate([
+            primaryView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            primaryView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            primaryView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+            primaryView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        ])
+    }
 
     @objc
     private func didTapSearch() {
---
+++ RickAndMorty/Views/Location/RMLocationView.swift
@@ -0,0 +1,76 @@
+//
+//  RMLocationView.swift
+//  RickAndMorty
+//
+//  Created by mathues barbosa on 31/01/23.
+//
+
+import UIKit
+
+final class RMLocationView: UIView {
+
+    private var viewModel: RMLocationViewViewModel? {
+        didSet {
+            spinner.startAnimating()
+            tableView.isHidden = false
+            tableView.reloadData()
+            UIView.animate(withDuration: 0.3) {
+                self.tableView.alpha = 1
+            }
+        }
+    }
+
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.translatesAutoresizingMaskIntoConstraints = false
+        table.alpha = 0
+        table.isHidden = true
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+
+    private let spinner: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView(style: .large)
+        spinner.hidesWhenStopped = true
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        return spinner
+    }()
+
+    // MARK: - Init
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .systemBackground
+        translatesAutoresizingMaskIntoConstraints = false
+        setUpViews()
+        addConstraints()
+        spinner.startAnimating()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("Unsupported")
+    }
+
+    private func setUpViews() {
+        addSubviews(tableView, spinner)
+    }
+
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            spinner.heightAnchor.constraint(equalToConstant: 100),
+            spinner.widthAnchor.constraint(equalToConstant: 100),
+            spinner.centerXAnchor.constraint(equalTo: centerXAnchor),
+            spinner.centerYAnchor.constraint(equalTo: centerYAnchor),
+
+            tableView.topAnchor.constraint(equalTo: topAnchor),
+            tableView.leftAnchor.constraint(equalTo: leftAnchor),
+            tableView.rightAnchor.constraint(equalTo: rightAnchor),
+            tableView.bottomAnchor.constraint(equalTo: bottomAnchor),
+        ])
+    }
+
+    public func configure(with viewModel: RMLocationViewViewModel) {
+        self.viewModel = viewModel
+    }
+
+}
